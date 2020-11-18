ac5a252705e453681a1c2779ba9db5da20041b99
victorlukas <70299777+victorlukas@users.noreply.github.com>
2020-11-18 16:43:40 -0300

    novo main
    não testei ainda com td

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-#esboço do codigo main.py
-
 import pygame, sys
 from random import randint
 from funcoes import *
 from funcoesfase1 import *
 from funcoesfase2 import *
 from funcoesfase3 import *
+from abertura import *
+from vitoria import *
 
 pygame.init()
-
-#com imagens novas da abertura (todos os 3 inimigos perseguindo o jogador)
 animation()
 
 jogo=1
@@ -21,10 +19,8 @@ while jogo==1:
     while nivel==1:
         fase1()
         if vidas<=0:
-            #esperar usuario apertar "s" ou "n"
-            #(como o jogo vai parar aqui a tela tb vai congelar)
             gameover1() #continuar a jogar ou não (se "n" o jogo fecha)
-            resetarfase1() #pode ser redefinindo as posiçoes e velocidades de td para as iniciais (dos inimigos sendo aleatórias)
+            resetarfase1()
         if pontos>=10:
             passoudefase() #continuar a jogar ou não (se "n" o jogo fecha)
             nivel+=1
@@ -48,8 +44,5 @@ while jogo==1:
         if pontos>=10:
             jogo=0 #vitoria na ultima fase
 
-#printar vitoria na ultima imagem da fase3 e opçao de fechar o jogo
-fim() #mais pygame.quit() e sys.exit()
-
-
-
+#printar vitoria
+fim()
